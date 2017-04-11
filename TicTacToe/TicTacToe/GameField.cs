f8d261aa9a2f8160b5d4eecbdd79697f8dfd76ae
maestro <max.belov.34@gmail.com>
2017-04-11 11:21:42 +0300

    tictactoe stub

---
+++ TicTacToe/TicTacToe/GameField.cs
@@ -0,0 +1,79 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace TicTacToe
+{
+    public partial class GameField : Form, IControler, IView
+    {
+        private GameModel model = new GameModel();
+        private Button[,] field;
+        Dictionary<GameModel.State, string> symbols = new Dictionary<GameModel.State, string>();
+
+        public GameField()
+        {
+            InitializeComponent();
+            symbols[GameModel.State.x] = "x";
+            symbols[GameModel.State.o] = "o";
+            symbols[GameModel.State.none] = "";
+            field = new Button[3, 3];
+            for (int i = 0; i < field.GetLength(0); i++)
+            {
+                for (int j = 0; j < field.GetLength(1); j++)
+                {
+                    Button b = new Button();
+                    // логические координаты кнопки
+                    b.Left = i * 50 + 10;
+                    b.Top = j * 50 + 10;
+                    b.Size = new Size(40, 40);
+                    b.Tag = new Point(i, j);
+                    b.Click += GameField_Click;
+                    // добавили на форму
+                    this.Controls.Add(b);
+                    // добавили в массив
+                    field[i, j] = b;
+                }
+            }
+
+
+            model.UpdateView += UpdateView;
+        }
+
+        void GameField_Click(object sender, EventArgs e)
+        {
+            Button b = (Button)sender;
+            Point p = (Point)b.Tag;
+            try
+            {
+                this.MakeMove(p.X, p.Y, model.CurrentMove);
+            }
+            catch (Exception ex)
+            {
+                MessageBox.Show(ex.Message);
+            }
+        }
+
+
+        public void UpdateView(GameModel model)
+        {
+            for (int i = 0; i < field.GetLength(0); i++)
+            {
+                for (int j = 0; j < field.GetLength(1); j++)
+                {
+                    field[i, j].Text = symbols[model.Field[i, j]];
+                }
+            }
+        }
+
+        public void MakeMove(int i, int j, GameModel.State side)
+        {
+            model.MakeMove(i, j, side);
+        }
+    }
+}
---
+++ TicTacToe/TicTacToe/GameModel.cs
@@ -0,0 +1,87 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace TicTacToe
+{
+    public class GameModel
+    {
+        public enum State
+        {
+            x,
+            o,
+            none
+        }
+
+        public State[,] Field { get; private set; }
+        public int CountStep { get; private set; }
+        public State Winner { get; private set; }
+        public bool GameOver { get; private set; }
+        public State CurrentMove { get; private set; }
+
+        public GameModel()
+        {
+            Field = new State[3, 3];
+            for (int i = 0; i < Field.GetLength(0); i++)
+            {
+                for (int j = 0; j < Field.GetLength(1); j++)
+                {
+                    Field[i, j] = State.none;
+                }
+            }
+            CountStep = 0;
+            Winner = State.none;
+            GameOver = false;
+            CurrentMove = State.x;
+        }
+
+
+        public delegate void UpdateViewDelegate(GameModel model);
+
+        public event UpdateViewDelegate UpdateView;
+
+        public void MakeMove(int i, int j, State side)
+        { 
+            if(GameOver)
+            {
+                throw new Exception("Game is already over!");
+            }
+
+            if(i < 0 || i > 2 || j < 0 || j > 2)
+            {
+                throw new IndexOutOfRangeException("Index out of field");
+            }
+
+            if(side != CurrentMove)
+            {
+                throw new Exception("it's not your turn");
+            }
+
+            if(Field[i, j] != State.none)
+            {
+                throw new Exception("Not empty field");
+            }
+
+            CountStep++;
+            Field[i, j] = side;
+
+            CheckForGameOver();
+            CurrentMove = side == State.o ? State.x : State.o;
+            UpdateView(this);
+        }
+
+        private void CheckForGameOver()
+        {
+            if (CountStep < 5) return;
+
+            throw new NotImplementedException();
+        }
+
+        
+        
+    }
+
+    
+}
