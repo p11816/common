c7352fc7fb8bec014ae35cc82d67eaac9474792e
cerebellum13 <nikita.maslovski13@gmail.com>
2017-04-12 11:53:25 +0300

    Izmenenie razmera poly

--- TicTacToe/TicTacToe/GameField.Designer.cs
+++ TicTacToe/TicTacToe/GameField.Designer.cs
@@ -28,9 +28,18 @@
         /// </summary>
         private void InitializeComponent()
         {
-            this.components = new System.ComponentModel.Container();
+            this.SuspendLayout();
+            // 
+            // GameField
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(300, 300);
+            this.Name = "GameField";
             this.Text = "Form1";
+            this.ResizeEnd += new System.EventHandler(this.GameField_ResizeEnd);
+            this.ResumeLayout(false);
+
         }
 
         #endregion
--- TicTacToe/TicTacToe/GameField.cs
+++ TicTacToe/TicTacToe/GameField.cs
@@ -15,10 +15,21 @@ namespace TicTacToe
         private GameModel model = new GameModel();
         private Button[,] field;
         Dictionary<GameModel.State, string> symbols = new Dictionary<GameModel.State, string>();
+         public struct OptionsSizeButton        // структура хранит нужные параметры для подсчета новых координат кнопак
+        {
+            public int hButt;            // высота кнопки
+            public int lButt;            // длина кнопки
+            public int dH1;              // расстояние между кнопками по вертикали
+            public int dL1;              // расстояние между кнопками по горизонтали
+            public int dH2;                // расстояние от крайней кнопки до границы формы по вертикали
+            public int dL2;                // расстояние от крайней кнопки до границы формы по вертикали
+        }
 
         public GameField()
         {
             InitializeComponent();
+            OptionsSizeButton sizeButt;
+            getOptionsSizeButton(out sizeButt);
             symbols[GameModel.State.x] = "x";
             symbols[GameModel.State.o] = "o";
             symbols[GameModel.State.none] = "";
@@ -29,9 +40,9 @@ namespace TicTacToe
                 {
                     Button b = new Button();
                     // логические координаты кнопки
-                    b.Left = i * 50 + 10;
-                    b.Top = j * 50 + 10;
-                    b.Size = new Size(40, 40);
+                    b.Left = sizeButt.dL2 + i * (sizeButt.dL1 + sizeButt.lButt);
+                    b.Top = sizeButt.dH2 + j * (sizeButt.dH1 + sizeButt.hButt); ;
+                    b.Size = new Size(sizeButt.lButt, sizeButt.hButt);
                     b.Tag = new Point(i, j);
                     b.Click += GameField_Click;
                     // добавили на форму
@@ -79,5 +90,39 @@ namespace TicTacToe
         {
             model.MakeMove(i, j, side);
         }
+
+        private void GameField_ResizeEnd(object sender, EventArgs e)
+        {
+            regdrawField();
+        }
+
+        private void regdrawField()
+        {
+            OptionsSizeButton sizeButt;
+            getOptionsSizeButton(out sizeButt);
+            for (int i = 0; i < field.GetLength(0); i++)
+            {
+                for (int j = 0; j < field.GetLength(1); j++)
+                {
+                    // логические координаты кнопки
+                    field[i, j].Left = sizeButt.dL2 + i * (sizeButt.dL1 + sizeButt.lButt);
+                    field[i, j].Top = sizeButt.dH2 + j * (sizeButt.dH1 + sizeButt.hButt); ;
+                    field[i, j].Size = new Size(sizeButt.lButt, sizeButt.hButt);
+                }
+            }
+        }
+
+        private void getOptionsSizeButton(out OptionsSizeButton newSise)
+        {   
+            int hForm = this.ClientSize.Height;
+            int lForm = this.ClientSize.Width;
+            newSise.hButt = (int)(hForm * 0.20);            // высота кнопки
+            newSise.lButt = (int)(lForm * 0.20);            // длина кнопки
+            newSise.dH1 = (int)(hForm * 0.03);              // расстояние между кнопками по вертикали
+            newSise.dL1 = (int)(lForm * 0.03);              // расстояние между кнопками по горизонтали
+            newSise.dH2 = (hForm - newSise.hButt * 3 - 2 * newSise.dH1) / 2;    // расстояние от крайней кнопки до границы формы по вертикали
+            newSise.dL2 = (lForm - newSise.lButt * 3 - 2 * newSise.dL1) / 2;    // расстояние от крайней кнопки до границы формы по вертикали
+        }
+
     }
 }
